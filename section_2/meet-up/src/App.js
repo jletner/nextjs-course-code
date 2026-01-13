90fb809dd76d8dd1126092fc3f81d10251c33b4e
Joe Letner <90421150+jletner@users.noreply.github.com>
2026-01-13 22:07:40 +0000

    Implement routing and add pages for All Meetups, New Meetup, and Favorites

--- section_2/meet-up/src/App.js
+++ section_2/meet-up/src/App.js
@@ -1,5 +1,24 @@
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact={true}>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup" exact={true}>
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites" exact={true}>
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
