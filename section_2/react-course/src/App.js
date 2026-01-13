8cda5c2b243dd9deb76a59741ab07b8bbdfbc114
Joe Letner <90421150+jletner@users.noreply.github.com>
2026-01-13 19:36:25 +0000

    Add card component structure and initial styles for the app

--- section_2/react-course/src/App.js
+++ section_2/react-course/src/App.js
@@ -2,6 +2,12 @@ function App() {
   return (
     <div>
       <h1>My Todo</h1>
+      <div className="card">
+        <h2>TITLE</h2>
+        <div>
+          <button>Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
