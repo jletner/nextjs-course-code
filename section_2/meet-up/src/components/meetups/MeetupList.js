3ba663fde87de062c56b75ce50b3d8a6c89ddf5c
Joe Letner <jrletner@gmail.com>
2026-01-17 21:52:16 -0600

    Implement AllMeetupsPage with DUMMY_DATA and create MeetupItem and MeetupList components with styles

---
+++ section_2/meet-up/src/components/meetups/MeetupList.js
@@ -0,0 +1,8 @@
+import classes from './MeetupList.module.css'
+
+const MeetupList = () => {
+    return (
+        <div>MeetupList</div>
+    )
+}
+export default MeetupList
--- section_2/meet-up/src/pages/AllMeetups.js
+++ section_2/meet-up/src/pages/AllMeetups.js
@@ -1,6 +1,15 @@
 import DUMMY_DATA from "../data/DUMMY_DATA";
 
 const AllMeetupsPage = () => {
-  return <div>All Meetups Page</div>;
+  return <section>
+    <h1>All Meetups</h1>
+    <ul>
+      {DUMMY_DATA.map((m) => {
+        return (
+          <li key={m.id}>{m.title}</li>
+        )
+      })}
+    </ul>
+  </section>;
 };
 export default AllMeetupsPage;
