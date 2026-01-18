92617455d51869be37d6f250fe5cce090a67c957
Joe Letner <jrletner@gmail.com>
2026-01-17 21:26:25 -0600

    Add MainNavigation component and styles for header and navigation

---
+++ section_2/meet-up/src/components/layout/MainNavigation.js
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import classes from './MainNavigation.module.css'
+
+const MainNavigation = () => {
+    return (
+        <header>
+            <div>React Meetups</div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to='/'>All Meetups</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default MainNavigation
