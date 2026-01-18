7b97db97c781f6d2623b7556eb2364687a6b71cb
Joe Letner <jrletner@gmail.com>
2026-01-17 22:23:19 -0600

    Simplify AllMeetupsPage by removing unnecessary mapping and directly using MeetupList with DUMMY_DATA

--- section_2/meet-up/src/pages/AllMeetups.js
+++ section_2/meet-up/src/pages/AllMeetups.js
@@ -4,13 +4,7 @@ import MeetupList from "../components/meetups/MeetupList";
 const AllMeetupsPage = () => {
   return <section>
     <h1>All Meetups</h1>
-    <ul>
-      {DUMMY_DATA.map((m) => {
-        return (
-          <MeetupList key={m.id} meetups={DUMMY_DATA} />
-        )
-      })}
-    </ul>
+    <MeetupList meetups={DUMMY_DATA} />
   </section>;
 };
 export default AllMeetupsPage;
