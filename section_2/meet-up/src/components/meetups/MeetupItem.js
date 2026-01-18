42608445c8df69985b215205cf3835214964bf50
Joe Letner <jrletner@gmail.com>
2026-01-17 22:08:31 -0600

    Add Card component to wrap MeetupItem for better styling and structure

--- section_2/meet-up/src/components/meetups/MeetupItem.js
+++ section_2/meet-up/src/components/meetups/MeetupItem.js
@@ -1,20 +1,24 @@
+import Card from '../ui/Card'
 import classes from './MeetupItem.module.css'
 
 const MeetupItem = ({ title, image, address, description }) => {
     return (
-        <li className={classes.item}>
-            <div className={classes.image}>
-                <img src={image} alt={title} />
-            </div>
-            <div className={classes.content}>
-                <h3>{title}</h3>
-                <address>{address}</address>
-                <p>{description}</p>
-            </div>
-            <div className={classes.actions}>
-                <button>To Favorites</button>
-            </div>
-        </li>
+        <Card>
+
+            <li className={classes.item}>
+                <div className={classes.image}>
+                    <img src={image} alt={title} />
+                </div>
+                <div className={classes.content}>
+                    <h3>{title}</h3>
+                    <address>{address}</address>
+                    <p>{description}</p>
+                </div>
+                <div className={classes.actions}>
+                    <button>To Favorites</button>
+                </div>
+            </li>
+        </Card>
     )
 }
 export default MeetupItem
